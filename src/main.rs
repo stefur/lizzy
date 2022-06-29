502c8b6e3fb0134b8b1038fbf7e0dce4ff1fd257
Stefan <stefan@furne.net>
2022-06-29 15:08:26 +0200

    A functioning handle for NameOwnerChange

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::time::Duration;
 
+use dbus::arg::TypeMismatchError;
 use dbus::blocking::LocalConnection;
 use dbus::message::MatchRule;
 use dbus::Message;
@@ -9,24 +10,66 @@ use dbus::MessageType::Signal;
 fn main() -> Result<(), Box<dyn Error>> {
     let conn = LocalConnection::new_session().expect("D-Bus connection failed!");
 
-    let rule = MatchRule::new()
+    let properties_rule = MatchRule::new()
         .with_path("/org/mpris/MediaPlayer2")
         .with_member("PropertiesChanged")
         .with_interface("org.freedesktop.DBus.Properties")
         .with_type(Signal);
 
-    conn.add_match(rule, |_: (), conn, msg| {
-        handle_message(&conn, &msg);
+    let nameowner_rule = MatchRule::new()
+        .with_path("/org/freedesktop/DBus")
+        .with_member("NameOwnerChanged")
+        .with_interface("org.freedesktop.DBus")
+        .with_sender("org.freedesktop.DBus")
+        .with_type(Signal);
+
+    conn.add_match(properties_rule, |_: (), conn, msg| {
+        handle_properties(&conn, &msg);
         true
     })
-    .expect("add_match failed!");
+    .expect("Adding properties_rule failed!");
+
+    conn.add_match(nameowner_rule, |_: (), _, msg| {
+        handle_nameowner(&msg);
+        true
+    })
+    .expect("Adding properties_rule failed!");
 
     loop {
         conn.process(Duration::from_millis(1000)).unwrap();
     }
 }
 
-fn handle_message(conn: &LocalConnection, msg: &Message) {
+struct NameOwnerChanged {
+    name: String,
+    old_name: String,
+    new_name: String,
+}
+
+fn handle_nameowner(msg: &Message) {
+    let nameowner = read_nameowner(msg).unwrap();
+
+    if nameowner.name == "org.mpris.MediaPlayer2.spotify" {
+        if nameowner.old_name == "" {
+            println!("Spotify opened. Probably not need to do anything here.")
+        }
+
+        if nameowner.new_name == "" {
+            println!("Spotify closed. Now we should clear the output.")
+        }
+    }
+}
+
+fn read_nameowner(msg: &Message) -> Result<NameOwnerChanged, TypeMismatchError> {
+    let mut iter = msg.iter_init();
+    Ok(NameOwnerChanged {
+        name: iter.read()?,
+        old_name: iter.read()?,
+        new_name: iter.read()?,
+    })
+}
+
+fn handle_properties(conn: &LocalConnection, msg: &Message) {
     if get_spotify_id(&conn).is_err() {
         return;
     }
