23cc200d314c0ce9377fa016409194fe79225804
stefur <stefan@furne.net>
2023-09-07 21:24:39 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-use core::panic;
+use core::time::Duration;
+use std::collections::HashMap;
 use std::error::Error;
-use std::time::Duration;
 
-use dbus::arg::{Iter, PropMap, RefArg, TypeMismatchError, Variant};
-use dbus::blocking::BlockingSender;
-use dbus::message::MatchRule;
-use dbus::strings::BusName;
-use dbus::Error as DBusError;
-use dbus::Message;
-use dbus::MessageType::Signal;
-use dbus::{arg, blocking::LocalConnection};
+use dbus::{
+    arg,
+    arg::{Iter, PropMap, RefArg, TypeMismatchError, Variant},
+    blocking::{BlockingSender, LocalConnection},
+    message::MatchRule,
+    strings::BusName,
+    Error as DBusError, Message,
+    MessageType::Signal,
+};
 
+use options::Arguments;
 mod options;
 
 struct NameOwnerChanged {
@@ -40,22 +42,22 @@ struct Output {
 impl Output {
     /// Create the output according to defined format
     fn new(song: Song, output_format: &str) -> Output {
-        let song_artist = song.artist.as_deref().unwrap_or("").to_string();
-        let song_title = song.title.as_deref().unwrap_or("").to_string();
+        let song_artist = song.artist.as_deref().unwrap_or("");
+        let song_title = song.title.as_deref().unwrap_or("");
 
-        let text = output_format
-            .replace("{{artist}}", &song_artist)
-            .replace("{{title}}", &song_title);
+        let now_playing = output_format
+            .replace("{{artist}}", song_artist)
+            .replace("{{title}}", song_title);
 
         Output {
             playbackstatus: song.playbackstatus,
-            now_playing: text, // The complete text
+            now_playing,
         }
     }
 
     /// Waybar doesn't like ampersand. So we replace them in the output string.
     fn escape_ampersand(&mut self) -> &mut Self {
-        self.now_playing = str::replace(&self.now_playing, "&", "&amp;");
+        self.now_playing = self.now_playing.replace("&", "&amp;");
         self
     }
 
@@ -100,94 +102,32 @@ fn main() -> Result<(), Box<dyn Error>> {
     conn.add_match(properties_rule, move |_: (), conn, msg| {
         // Start by checking if the signal is indeed from the mediaplayer we want
         if is_mediaplayer(conn, msg, &properties_opts.mediaplayer) {
-            // Unpack the song received from the signal to create an output
-            let contents = parse_message(msg);
-
-            let song: Song = match contents {
-                Some(Contents::Metadata { artist, title }) => Song {
-                    artist: artist.unwrap_or_default().get(0).cloned(),
-                    title,
-                    playbackstatus: get_property(conn, msg.sender(), "PlaybackStatus")
-                        .0
-                        .unwrap_or_default(),
-                },
-                Some(Contents::PlaybackStatus(status)) => {
-                    let metadata = get_property(conn, msg.sender(), "Metadata");
-                    Song {
-                        artist: metadata.0,
-                        title: metadata.1,
-                        playbackstatus: status.unwrap_or_default(),
-                    }
-                }
-                None => {
-                    // Just return if we fail to find a match.
-                    // For now this assumes messages we're not interested in.
-                    return true;
-                }
-            };
-
-            // Create an output from the song
-            let mut output = Output::new(song, &properties_opts.format);
-
-            // Customize the output and send it
-            output.escape_ampersand().send();
-        } else {
-            // First we check that our mediaplayer is even running and that the autotoggle flag is used
-            if let (Some(mediaplayer), true) = (
-                query_id(conn, &properties_opts.mediaplayer),
-                properties_opts.autotoggle,
-            ) {
-                // If the other mediaplayer wasn't closed after sending its signal we parse the message
-                let other_media = parse_message(msg);
-                let status: String = match other_media {
-                    Some(Contents::PlaybackStatus(status)) => status.unwrap_or_default(),
-                    // If we receive a message containing only metadata, we manually check the playback status
-                    // since a message with metadata also means me is playing
-                    Some(Contents::Metadata { .. }) => {
-                        get_property(conn, msg.sender(), "PlaybackStatus")
-                            .0
-                            .unwrap_or_default()
-                    }
-                    // Ignore anything else
-                    _ => return true,
-                };
-
-                match status.as_str() {
-                    "Playing" => {
-                        toggle_playback(conn, &mediaplayer, "Pause");
-                    }
-                    "Paused" | "Stopped" | "" => {
-                        toggle_playback(conn, &mediaplayer, "Play");
-                    }
-                    _ => {
-                        println!("Failed to match the playbackstatus");
-                        return true;
-                    }
-                }
-            }
+            handle_valid_mediaplayer_signal(conn, msg, &properties_opts);
+        } else if should_toggle_playback(conn, &properties_opts) {
+            toggle_playback_if_needed(conn, msg, &properties_opts);
         }
         true
     })?;
 
     // Handles any incoming messages when a nameowner has changed.
     conn.add_match(nameowner_rule, move |_: (), _, msg| {
-        // Not a very pretty solution, but if we listen to signals coming from all mediaplayers,
-        // we never clear the output to avoid clearing the output in error due to mediaplayers closing
+        // Check if we should listen to all mediaplayers
         if nameowner_opts.mediaplayer.is_empty() {
             return true;
         }
 
         if let Ok(nameowner) = read_nameowner(msg) {
-            // If mediaplayer has been closed, clear the output by sending an empty string
+            // If the mediaplayer has been closed, clear the output
             if nameowner
                 .name
                 .to_lowercase()
-                .contains(nameowner_opts.mediaplayer.as_str())
+                .contains(&nameowner_opts.mediaplayer)
                 && nameowner.new_name.is_empty()
             {
                 println!();
             }
         }
+
         true
     })?;
 
@@ -215,8 +155,7 @@ fn parse_message(msg: &Message) -> Option<Contents> {
     match read_msg {
         Ok(read_msg) => {
             // Get the HashMap from the second argument, which contains the relevant info
-            let map = read_msg.1;
-
+            let map: HashMap<String, Variant<Box<dyn RefArg>>> = read_msg.1;
             // The string that tells us what kind of contents is in the message
             if let Some(contents) = map.keys().next() {
                 match contents.as_str() {
@@ -258,16 +197,77 @@ fn parse_message(msg: &Message) -> Option<Contents> {
 }
 
 /// Calls a method on the interface to play or pause what is currently playing
-fn toggle_playback(conn: &LocalConnection, mediaplayer: &String, cmd: &str) {
-    if let Ok(message) = dbus::Message::new_method_call(
+fn toggle_playback(conn: &LocalConnection, mediaplayer: &str, cmd: &str) {
+    let message = dbus::Message::new_method_call(
         mediaplayer,
         "/org/mpris/MediaPlayer2",
         "org.mpris.MediaPlayer2.Player",
         cmd,
-    ) {
-        match conn.send_with_reply_and_block(message, Duration::from_millis(5000)) {
+    );
+
+    match message {
+        Ok(message) => match conn.send_with_reply_and_block(message, Duration::from_millis(5000)) {
             Ok(_) => (),
             Err(err) => eprintln!("Failed to toggle playback. Error: {}", err),
+        },
+        Err(err) => eprintln!("Failed to create method call. Error: {}", err),
+    }
+}
+
+fn handle_valid_mediaplayer_signal(
+    conn: &LocalConnection,
+    msg: &Message,
+    properties_opts: &Arguments,
+) {
+    let contents = parse_message(msg);
+
+    let song = match contents {
+        Some(Contents::Metadata { artist, title }) => Song {
+            artist: artist.unwrap_or_default().get(0).cloned(),
+            title,
+            playbackstatus: get_property(conn, msg.sender(), "PlaybackStatus")
+                .0
+                .unwrap_or_default(),
+        },
+        Some(Contents::PlaybackStatus(status)) => {
+            let metadata = get_property(conn, msg.sender(), "Metadata");
+            Song {
+                artist: metadata.0,
+                title: metadata.1,
+                playbackstatus: status.unwrap_or_default(),
+            }
+        }
+        None => return, // Ignore messages with no valid content
+    };
+
+    let mut output = Output::new(song, &properties_opts.format);
+    output.escape_ampersand().send();
+}
+
+fn should_toggle_playback(conn: &LocalConnection, properties_opts: &Arguments) -> bool {
+    properties_opts.autotoggle && query_id(conn, &properties_opts.mediaplayer).is_some()
+}
+
+fn toggle_playback_if_needed(conn: &LocalConnection, msg: &Message, properties_opts: &Arguments) {
+    let other_media = parse_message(msg);
+    let status = match other_media {
+        Some(Contents::PlaybackStatus(status)) => status.unwrap_or_default(),
+        Some(Contents::Metadata { .. }) => get_property(conn, msg.sender(), "PlaybackStatus")
+            .0
+            .unwrap_or_default(),
+        _ => return, // Ignore messages with no valid content
+    };
+
+    let mediaplayer = match query_id(conn, &properties_opts.mediaplayer) {
+        Some(mediaplayer) => mediaplayer,
+        None => return, // No valid mediaplayer found
+    };
+
+    match status.as_str() {
+        "Playing" => toggle_playback(conn, &mediaplayer, "Pause"),
+        "Paused" | "Stopped" | "" => toggle_playback(conn, &mediaplayer, "Play"),
+        _ => {
+            println!("Failed to match the playbackstatus");
         }
     }
 }
@@ -279,61 +279,54 @@ fn get_property(
     property: &str,
 ) -> (Option<String>, Option<String>) {
     if let Some(mediaplayer) = busname {
+        let interface = "org.mpris.MediaPlayer2.Player";
+        let path = "/org/mpris/MediaPlayer2";
         let message = dbus::Message::call_with_args(
             mediaplayer,
-            "/org/mpris/MediaPlayer2",
+            path,
             "org.freedesktop.DBus.Properties",
             "Get",
-            ("org.mpris.MediaPlayer2.Player", property),
+            (interface, property),
         );
 
-        let reply: Result<Message, DBusError> =
-            conn.send_with_reply_and_block(message, Duration::from_millis(5000));
-
-        match (reply, property) {
-            (Ok(reply), "PlaybackStatus") => {
-                // Unpack the playback status, or return an empty string which is rare, but happens
-                let result: Variant<String> = reply.read1().unwrap_or(Variant(String::new()));
-
-                (Some(result.0), None)
-            }
-            (Ok(reply), "Metadata") => {
-                let metadata: Result<Variant<PropMap>, TypeMismatchError> = reply.read1();
-
-                match metadata {
-                    Ok(metadata) => {
-                        let properties: PropMap = metadata.0;
-                        let title: Option<String> =
-                            arg::prop_cast(&properties, "xesam:title").cloned();
-                        let artist: Option<Vec<String>> =
-                            arg::prop_cast(&properties, "xesam:artist").cloned();
-                        let artist = artist.unwrap_or_default().get(0).cloned();
-                        let result: (Option<String>, Option<String>) = (artist, title);
-
-                        result
+        let (artist, title) =
+            match conn.send_with_reply_and_block(message, Duration::from_millis(5000)) {
+                Ok(reply) => match property {
+                    "PlaybackStatus" => {
+                        let result: Variant<String> =
+                            reply.read1().unwrap_or_else(|_| Variant(String::new()));
+                        (Some(result.0), None)
                     }
-
-                    Err(err) => {
-                        // We print an error message if there is an issue with parsing the reply.
-                        // Returning none, no need to panic. This should rarely, if ever, happen.
-                        // Error might still be useful though.
-                        eprintln!("Failed to get metadata. Error: {}", err);
-                        (None, None)
+                    "Metadata" => {
+                        let metadata: Result<Variant<PropMap>, TypeMismatchError> = reply.read1();
+                        match metadata {
+                            Ok(metadata) => {
+                                let properties: PropMap = metadata.0;
+                                let title: Option<String> =
+                                    arg::prop_cast(&properties, "xesam:title").cloned();
+                                let artist: Option<Vec<String>> =
+                                    arg::prop_cast(&properties, "xesam:artist").cloned();
+                                (artist.unwrap_or_default().get(0).cloned(), title)
+                            }
+                            Err(err) => {
+                                eprintln!("Failed to get metadata. Error: {}", err);
+                                (None, None)
+                            }
+                        }
                     }
-                }
-            }
-            (Ok(_reply), &_) => (None, None),
-            // Return none in case we receive an error.
-            // This is usually cause the mediaplayer closed after the signal was sent.
-            (Err(_err), _) => (None, None),
-        }
+                    _ => (None, None),
+                },
+                Err(_) => (None, None),
+            };
+
+        (artist, title)
     } else {
         (None, None)
     }
 }
 
 /// Create a query with a method call to ask for the ID of the mediaplayer
-fn query_id(conn: &LocalConnection, mediaplayer: &String) -> Option<String> {
+fn query_id(conn: &LocalConnection, mediaplayer: &str) -> Option<String> {
     let query = dbus::Message::call_with_args(
         "org.freedesktop.DBus",
         "/",
@@ -348,34 +341,26 @@ fn query_id(conn: &LocalConnection, mediaplayer: &String) -> Option<String> {
 
     match reply {
         // If we get a reply, we unpack the ID from the message and return it
-        Ok(reply) => {
-            let mediaplayer_id: String = reply.read1().unwrap_or_default();
+        Ok(reply) => Some(reply.read1().unwrap_or_default()),
 
-            Some(mediaplayer_id)
-        }
-
-        // If the message is not from the mediaplayer we're listening to we'll receive an error in return, which is fine, so return false
-        Err(_reply) => None,
+        // If the message is not from the mediaplayer we're listening to, we'll receive an error in return, which is fine, so return None
+        Err(_) => None,
     }
 }
 
 /// Check if the sender of a message is the mediaplayer we're listening to
-fn is_mediaplayer(conn: &LocalConnection, msg: &Message, mediaplayer: &String) -> bool {
+fn is_mediaplayer(conn: &LocalConnection, msg: &Message, mediaplayer: &str) -> bool {
     // If mediaplayer option is blank, we listen to all incoming signals and thus return true
     if mediaplayer.is_empty() {
         return true;
     }
 
     // Extract the sender of our incoming message
-    let sender_id = msg
-        .sender()
-        .unwrap_or_else(|| String::new().into())
-        .to_string();
+    let sender_id = msg.sender().map_or(String::new(), |s| s.to_string());
 
-    // Send the query and await the reply
-    if let Some(mediaplayer_id) = query_id(conn, mediaplayer) {
-        sender_id == mediaplayer_id
-    } else {
-        false
+    // Check if the sender matches the specified mediaplayer
+    match query_id(conn, mediaplayer) {
+        Some(mediaplayer_id) => sender_id == mediaplayer_id,
+        None => false,
     }
 }
